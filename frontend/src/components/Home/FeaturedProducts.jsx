dfd5cdf2e5e03e5e2a288a614ccfad7d33676b5a
Humera Khan <hkhan@dnaofsafety.net>
2024-08-11 17:58:51 +0530

    [Integrated] API to display featured product data using useData hook (dev)

--- frontend/src/components/Home/FeaturedProducts.jsx
+++ frontend/src/components/Home/FeaturedProducts.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import './FeaturedProducts.css'
 import ProductCard from '../Products/ProductCard';
+import useData from '../../hooks/useData';
+import ProductCardSkeleton from '../Products/ProductCardSkeleton';
 
 const FeaturedProducts = () => {
+  const {data, error, isLoading} = useData("/products/featured")
+  const skeletons = Array(3).fill(0);
   return (
     <section className="featured_products">
         <h2>Featured Products</h2>
         <div className='align_center featured_product_list'>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+          {error && <em className='form_error'>{error}</em>}
+          {data && data?.map(product => <ProductCard key={product?._id} product={product}/>
+            )}
+          {isLoading && skeletons.map(skeleteon => <ProductCardSkeleton key={skeleteon}/>)}
         </div>
 
     </section>
--- frontend/src/components/Home/HomePage.jsx
+++ frontend/src/components/Home/HomePage.jsx
@@ -10,10 +10,10 @@ const HomePage = () => {
   return (
     <div>
         <HeroSection title="Buy iphone 14 pro" subtitle="Experience the power of latest iphone 14 with most pro camera ever"
-            link="/" image={iphone}/>
+            link="/product/66ad095ab41f49410646358e" image={iphone}/>
         <FeaturedProducts/>
         <HeroSection title="Build the ultimate setup" subtitle="You can add studio display and color-matched magic accessories"
-            link="/" image={mac}/>
+            link="/product/66ad095ab41f494106463596" image={mac}/>
     </div>
   )
 }
