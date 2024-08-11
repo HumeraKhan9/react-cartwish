ccdad5568fdf82c396c99304319d4e43e6b276e4
Humera Khan <hkhan@dnaofsafety.net>
2024-08-11 14:32:10 +0530

    [Integrated] Order api to display the data (dev)

--- frontend/src/components/MyOrder/MyOrderPage.jsx
+++ frontend/src/components/MyOrder/MyOrderPage.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import './MyOrderPage.css'
 import Table from '../Common/Table';
+import useData from '../../hooks/useData';
+import Loader from "../Common/Loader";
 
 const MyOrderPage = () => {
+  const {data: orders, error, isLoading} = useData("/order")
+  const getProductString = order => {
+    const productStringArray = order.products.map(p => `${p?.product?.title}(${p?.quantity})`)
+    return productStringArray.join(", ")
+  }
   return (
     <section className="align_center myorder_page">
+      {isLoading && <Loader/>}
+      {error && <em className='form_error'>{error}</em>}
+      {orders && (
         <Table headings={["Order", "Products", "Total", "Status"]}>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>power bank</td>
-                    <td>$1299</td>
-                    <td>Shipped</td>
-                </tr>
-            </tbody>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={order?._id}>
+                <td>{index + 1}</td>
+                <td>{getProductString(order)}</td>
+                <td>${order?.total}</td>
+                <td>{order?.status}</td>
+              </tr>
+            ))}
+          </tbody>
         </Table>
+      )}
     </section>
-  )
+  );
 }
 
 export default MyOrderPage
